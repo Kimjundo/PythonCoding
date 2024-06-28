0053a9d2b97adfb0a7550528d7009ade48257368
Kinjundo <wnseh0613@naver.com>
2024-06-28 23:24:33 +0900

    첫번째 문재

--- 1문제.py
+++ 1문제.py
@@ -1,1 +1,9 @@
-i = 19
+# 두수의차 정수 num1 과 num2 가 주어질때, num1에 서 num2를 뺀 값을 return 하도록 solution함수를 완성 하시오
+def solution(num1, num2):
+    answer = num1 - num2
+    return answer 
+num1 = int(input('num1을 입력해 주세요'))
+num2 = int(input('num2을 입력해 주세요'))
+
+
+print(num1 + num2 + solution(num1,num2))
