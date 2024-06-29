9456d58013139d6db48cf2f4cbb29b5e7be513ba
Kinjundo <wnseh0613@naver.com>
2024-06-29 21:39:01 +0900

    2번째 문제

---
+++ first.py
@@ -0,0 +1,8 @@
+def solution(num1, num2):
+    answer = num1 - num2
+    return answer 
+num1 = int(input('num1을 입력해 주세요'))
+num2 = int(input('num2을 입력해 주세요'))
+
+
+print(num1 + num2 + solution(num1,num2))
